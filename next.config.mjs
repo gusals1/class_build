8aa98962b9e80bab5551dd95151f79d4c976d694
qkrgus3318@gmail.com <qkrgus3318@gmail.com>
2024-08-01 11:53:27 +0900

    분 기 배 포

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // output: "export",
+  output: "export",
+  generateBuildId: () => "class_build_deploy",
 };
-
+// exportPathMap:"" 으로 페이지를 제외 시키면 서버 사이드 렌더링 되는 요소를 out폴더에 포함되지 않게 막을수 있다.
 export default nextConfig;
